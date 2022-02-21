8dab2e95298619ac105164fc3678efbe813883bb
pgman <olj611@gmail.com>
2022-02-21 13:05:54 +0900

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 setInterval(() => {
 	const container = document.querySelector('.ytp-autonav-endscreen-countdown-container');	
-	if(container && container.style.display && container.style.display !== 'none' ) {
+	if(container && container.style.display !== 'none' ) {
 		const a = document.querySelector('a.ytp-autonav-endscreen-link-container');	
 		if(a) { a.click(); }
 	}
